2eba8af29d4a4cf35b8fe2a6b8d1c6769cbf3447
Matheus Danemberg <matheusdanemberg@gmail.com>
2017-11-24 12:22:57 -0200

    init

---
+++ migrations/001-initial.sql
@@ -0,0 +1,27 @@
+--------------------------------------------------------------------------------
+-- Up
+--------------------------------------------------------------------------------
+
+CREATE TABLE Users (
+  id       INTEGER PRIMARY KEY,
+  name     TEXT    NOT NULL,
+  email    TEXT    NOT NULL,
+  password TEXT    NOT NULL
+);
+
+CREATE TABLE Tweets (
+  id        INTEGER PRIMARY KEY,
+  userId    INTEGER NOT NULL,
+  text TEXT NOT NULL,
+  CONSTRAINT Tweets_fk_userId FOREIGN KEY (userId)
+    REFERENCES Users (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE INDEX Tweets_ix_userId ON Tweets (userId);
+--------------------------------------------------------------------------------
+-- Down
+--------------------------------------------------------------------------------
+
+DROP INDEX Tweets_ix_userId;
+DROP TABLE Tweets;
+DROP TABLE Users;
